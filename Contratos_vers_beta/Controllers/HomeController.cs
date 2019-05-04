6ee0a66550aa8b03d64cbbfddc388bc57b6652c1
Miguel Ramirez <mike__221@hotmail.com>
2019-05-03 19:10:52 -0500

    Cambios PDF

--- Contratos_vers_beta/Controllers/HomeController.cs
+++ Contratos_vers_beta/Controllers/HomeController.cs
@@ -377,6 +377,30 @@ namespace Contratos_vers_beta.Controllers
 
             return File(PDFStream, "application/pdf");
         }
+
+        [HttpGet]
+        public ActionResult CreaPDF()
+        {
+            ExportPDF pdf = new ExportPDF();
+            pdf.CreatePdf("C:/Users/Rodrigo/Desktop/Contratos-Alpha/hola4" + DateTime.Now.Millisecond +".pdf", db.Contratos.ToList());
+            return View("ViewContratos");
+        }
+
+        [HttpPost]
+        public ActionResult Excel_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Pdf_Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
     }
 }
 
---
+++ Contratos_vers_beta/Models/ExportPDF.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Kernel.Pdf;
+using System.IO;
+using System.ComponentModel.DataAnnotations.Schema;
+using iText.Kernel.Geom;
+
+namespace Contratos_vers_beta.Models
+{
+    [NotMapped]
+    public class ExportPDF
+    {
+        public void CreatePdf(string dest, List<Contratos> contratos)
+        {
+            //Initialize PDF writer
+            PdfWriter writer = new PdfWriter(dest);
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(writer);
+            // Initialize document
+            Document document = new Document(pdf, PageSize.A4.Rotate());
+            
+            //Add paragraph to the document
+            Table tabla = new Table(16);
+            foreach (var item in contratos)
+            {
+                tabla.AddCell(new Paragraph(item.FECHA.ToString() != "" ? item.FECHA.Value.ToShortDateString() : ""));
+                        
+                tabla.AddCell(new Paragraph(item.EMPRESA));
+                tabla.AddCell(new Paragraph(item.APODERADO));
+                tabla.AddCell(new Paragraph(item.FIRMADO ? "Sí" : "No")).SetMaxWidth(10);
+                tabla.AddCell(new Paragraph(item.EMPRESA_1));
+                tabla.AddCell(new Paragraph(item.APODERADO_1));
+                tabla.AddCell(new Paragraph(item.FIRMADO_1? "Sí" : "No")).SetMaxWidth(10);
+
+                //tabla.AddCell(new Paragraph(item.EMPRESA_2));
+                //tabla.AddCell(new Paragraph(item.APODERADO_2));
+                //tabla.AddCell(new Paragraph(item.FIRMADO_2.ToString()));
+                //tabla.AddCell(new Paragraph(item.CONTRAPRESTACION_IVA_INCLUIDO));
+                //tabla.AddCell(new Paragraph(item.VIGENCIA_TAL_CUAL_ESTIPULA_EL_CONTRATO));
+                //tabla.AddCell(new Paragraph(item.ORIGINAL_O_COPIA.ToString()));
+                //tabla.AddCell(new Paragraph(item.ANIO_DE_FIRMA));
+                //tabla.AddCell(new Paragraph(item.OBSERVACIONES));
+            }
+
+            document.Add(tabla);
+            //Close document
+            document.Close();
+        }
+    }
+}
